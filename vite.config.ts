625aba958e49bfe67c6366aaac333810b05b2ca6
Asf rzhn <asfarezhan67@gmail.com>
2025-08-09 20:22:43 +0700

    Initial commit: Proyek Analisis Isu Latsar

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/tugas_analisis_isu_latsar/', 
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
